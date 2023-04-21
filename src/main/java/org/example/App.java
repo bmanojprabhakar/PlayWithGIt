5b9c054e37a260145602cd6932cf4f26b0f988f8
Manoj Prabhakar <manoj.prabhakar@vonage.com>
2023-04-22 00:17:42 +0530

    feature: learn merge pull request

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,1 +1,1 @@
-package org.example;/** * Hello world! */public class App {    public static void main(String[] args) {        System.out.println("Hello World!");    }}
+package org.example;/** * Hello world! */public class App {    public static void main(String[] args) {        System.out.println("Feature branch changes to check merge and squash");        System.out.println("Hello World!");    }}
